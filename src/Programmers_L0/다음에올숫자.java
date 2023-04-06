706aed9f534511910b8f3acd5da33641a079cc79
SuyeonSun <suninisy@gmail.com>
2023-04-06 10:24:22 +0900

    문자열과 배열의 길이

---
+++ src/Programmers_L0/다음에올숫자.java
@@ -0,0 +1,12 @@
+package Programmers_L0;
+
+public class 다음에올숫자 {
+    public static void main(String[] args) {
+        // 문자열의 길이
+        String testString = "abc ";
+        System.out.println(testString.length());
+        // 배열의 길이
+        String[] testArr = {"a", "b", "c"};
+        System.out.println(testArr.length);
+    }
+}
--- src/Programmers_L0/옹알이.java
+++ src/Programmers_L0/옹알이.java
@@ -2,7 +2,6 @@ package Programmers_L0;
 
 public class 옹알이 {
     public static void main(String[] args) {
-        // 몰랐던 개념
         String replaceTest = "리플레이스 테스트 리플레이스";
 
         // replace()
@@ -19,7 +18,7 @@ public class 옹알이 {
         String result3 = replaceTest.replaceFirst("리플레이스", "replace");
         System.out.println(result3);
 
-        // replaceFirst() 예제
+        // replaceFirst() 예제 풀이1
         String[] babblingInput = {"aya", "yee", "u", "maa", "wyeoo"};
         String[] babblingArr = {"aya", "ye", "woo", "ma"};
         int answer = 0;
@@ -30,6 +29,20 @@ public class 옹알이 {
             }
             if ("" == bab.replace(" ", "")) {
                 answer++;
+            }
+        }
+        System.out.println(answer);
+
+        // replaceFirst() 예제 풀이2
+        answer = 0;
+        for (int i=0; i<babblingInput.length; i++) {
+            babblingInput[i] = babblingInput[i].replace("aya", "1");
+            babblingInput[i] = babblingInput[i].replace("woo", "1");
+            babblingInput[i] = babblingInput[i].replace("ye", "1");
+            babblingInput[i] = babblingInput[i].replace("ma", "1");
+            babblingInput[i] = babblingInput[i].replace("1", "");
+            if (babblingInput[i].isEmpty()) {
+                answer ++;
             }
         }
         System.out.println(answer);
