a5b6095ba2bca9c65a9b8ad63cd85ae73ac72ddf
SuyeonSun <suninisy@gmail.com>
2023-04-10 16:21:52 +0900

    toCharArray() 연습

---
+++ src/main/java/Programmers/L1/ToCharArrayPractice.java
@@ -0,0 +1,9 @@
+package Programmers.L1;
+
+public class ToCharArrayPractice {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        char[] strArr = str.toCharArray();
+        System.out.println(strArr[0]);
+    }
+}
