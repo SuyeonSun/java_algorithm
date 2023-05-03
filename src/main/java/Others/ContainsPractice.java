eff150b0a4ff1e64a864da4e5c68009cf16b3f00
SuyeonSun <suninisy@gmail.com>
2023-05-04 08:49:59 +0900

    min,max 함수 및 배열의 최소, 최대 값

--- src/main/java/Others/ContainsPractice.java
+++ src/main/java/Others/ContainsPractice.java
@@ -10,8 +10,9 @@ public class ContainsPractice {
         str = str.replace("3", "");
         System.out.println(str);
 
-        // ===== 2. substring =====
+        // ===== 2. str.substring(i, i+length) =====
         // startIndex부터 끝까지 문자열 return
+        // length 만큼 출력
         String str2 = "abc";
         // str2 = str2.substring(0, 2);
         // str2 = str2.substring(1, 2);
@@ -48,5 +49,27 @@ public class ContainsPractice {
         array[0] = park[0].toCharArray();
         System.out.println(array[0]);
         System.out.println(array[0][0]);
+
+        // ===== 8. toCharArray() =====
+
+        // ===== 9. Integer.parseInt(num) =====
+        String t = "3141592";
+        for (int i = 0; i < t.length() - 3 + 1; i++) {
+            int num = Integer.parseInt(t.substring(i, i + 3));
+        }
+
+        // ===== 10. 배열의 최대, 최소 값 =====
+        int[] arr2 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        Arrays.sort(arr2);
+
+        // ===== 11. =====
+        int[] arr3 = {1, 3, 5, 7, 9};
+        int min = 999;
+        int max = -1;
+        for (int i : arr3) {
+            min = Math.min(i, min);
+            max = Math.max(i, max);
+        }
+        System.out.println(min + " " + max);
     }
 }
