41829318439020c146749763d21d2bf32e867f23
SuyeonSun <suninisy@gmail.com>
2023-05-07 10:01:33 +0900

    reference 추가

--- src/main/java/Others/dfsPractice.java
+++ src/main/java/Others/dfsPractice.java
@@ -1,5 +1,6 @@
 package Others;
 
+// https://codingnojam.tistory.com/44
 public class dfsPractice {
     // 그래프 연결 상태
     static int[][] graph = {{}, {2,3,8}, {1,6,8}, {1,5}, {5,7}, {3,4,7}, {2}, {4,5}, {1,2}};
