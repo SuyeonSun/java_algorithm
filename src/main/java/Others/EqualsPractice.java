e489f4a225cd5382d8181b70d98ec311562ec159
SuyeonSun <suninisy@gmail.com>
2023-05-15 13:52:12 +0900

    ==와 equals의 비교

--- src/main/java/Others/EqualsPractice.java
+++ src/main/java/Others/EqualsPractice.java
@@ -5,5 +5,13 @@ public class EqualsPractice {
         String str = "java";
         System.out.println(str.charAt(0) == 'j');
         // System.out.println(str.charAt(0) == "j"); // Operator '==' cannot be applied to 'char', 'java.lang.String'
+
+        // == 연산자는 주소값 비교
+        // String class의 equals 메소드는 값 자체 비교
+        String str1 = "string";
+        String str2 = new String("string");
+        System.out.println(str1 == str2); // false
+        System.out.println(str1.equals(str2)); // true
+
     }
 }
