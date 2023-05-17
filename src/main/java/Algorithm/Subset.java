f917ca99b75505249025bb2d1b2e812d3638d987
SuyeonSun <suninisy@gmail.com>
2023-05-17 11:17:27 +0900

    부분집합 수정

--- src/main/java/Algorithm/Subset.java
+++ src/main/java/Algorithm/Subset.java
@@ -1,11 +1,22 @@
 package Algorithm;
 
+// 입력값
+// 3
+// 1 2 3
+
+// 출력값
+// 123
+// 12
+// 13
+// 1
+// 23
+// 2
+// 3
+
 public class Subset {
     public static void main(String[] args) {
-        // 2
-        // 1 2
-        int N = 2;
-        int[] arr = {1, 2};
+        int N = 3;
+        int[] arr = {1, 2, 3};
         boolean[] isSelected = new boolean[N];
         generateSubset(0, N, arr, isSelected);
     }
@@ -13,7 +24,9 @@ public class Subset {
     public static void generateSubset(int cnt, int N, int[] arr, boolean[] isSelected) {
         if (cnt == N) {
             for (int i = 0; i < N; i++) {
-                System.out.print(isSelected[i] ? arr[i] : "X");
+                if (isSelected[i]) {
+                    System.out.print(arr[i]);
+                }
             }
             System.out.println();
             return;
