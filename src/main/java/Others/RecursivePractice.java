c5aa776a319b4f0269861ad9c65c52d628b13ec3
SuyeonSun <suninisy@gmail.com>
2023-05-18 11:07:50 +0900

    재귀함수에서의 static 변수 사용

---
+++ src/main/java/Others/RecursivePractice.java
@@ -0,0 +1,19 @@
+package Others;
+
+public class RecursivePractice {
+    static int cnt;
+
+    public static void main (String[] args) {
+        // int cnt = 0;
+        cnt = 0;
+        recursive();
+        System.out.println(cnt);
+    }
+
+    // public static void recursive (int cnt) {
+    public static void recursive () {
+        if (cnt == 3) return;
+        cnt++;
+        recursive();
+    }
+}
